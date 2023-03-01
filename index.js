a734281ad7669f1f5fbc08066af9ffceeca09d3e
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-01 23:28:02 +0500

    if , esle

---
+++ index.js
@@ -0,0 +1,18 @@
+// yomg'ir,qor ,issiq ,soviq.
+
+
+let hozir="jhdj"
+
+if(hozir === "qor"){
+    console.log("havo sovuq,issiqroq kiyinib oling");
+}
+ if(hozir === "yomg'ir"){
+    console.log("havo sovuq, soyobon oling qalimroq kiying");
+
+}
+ if(hozir === "issiq"){
+    console.log("havo issiq , yengilroq kiyining");
+}
+else{
+    console.log("havo sovuq , qalin kiying");
+}
