95b4d5240b7eddaac4a2fc126b9093268a1ea7e9
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-01 21:40:00 +0500

    if else

--- review.js
+++ review.js
@@ -1,1 +1,15 @@
-git Infinity
+//let a=1
+//b=2
+//if (a+b<4){
+  //  console.log("below");}
+//else{
+  //  console.log("over");}
+//if('0'){
+  //  alert('Hello');}
+
+//============
+
+2-masala
+let name='ESMASscript';
+if (name === 'ESMASscript') console.log('right');
+else console.log('You dont know ESMASscript');
