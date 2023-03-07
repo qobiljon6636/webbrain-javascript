ab32da522c9f46ab3e4be8daffc796f4439a004b
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-07 15:18:31 +0500

    function

--- review.js
+++ review.js
@@ -9,13 +9,80 @@
 
 //============
 
-2-masala
+// 2-masala
 // let name='ESMASscript';
 // if (name === 'ESMASscript') console.log('right');
 // else console.log('You dont know ESMASscript');
 
 //===========
-3-masala
-let a=2;
-let x=1+(a *=2);
-console.log(x);
+// 3-masala
+// let a=2;
+// let x=1+(a *=2);
+// console.log(x);
+
+
+
+
+
+
+
+// function checkAge(age) {
+//     if (age>18){
+//     return true;
+//     }else{
+//         return confirm('Did parents allow you?')
+//     }
+// }
+
+// function checkAge(age) {
+//      age > 18 ? return true : return confirm('Did parents allow you?')
+//     }
+
+
+// ===================================
+// a=1
+// b=3
+// function min(){
+//     if (a>b){
+//         return '3';
+//     }else{
+//         return '1';
+//     }
+// }
+// console.log(min());
+
+
+//========================
+// a=5
+// b=5
+// function pow(){
+//     return(a ** b);
+// }
+// console.log(pow());
+
+//======================
+
+// function ask(question,yes,no){
+//     if (confirm(question),no());
+//     else no()
+// }
+
+
+// ask(
+//     "Do you agree?"
+//     function() {alert("you agreed");},
+//     function() {alert("you canceled the exection");}
+// )
+
+// arrowFanc=>
+
+// function arrowFanc(question,yes,no){
+//     if (confirm(question),no());
+//     else no()
+// }
+
+// arrowFanc(
+//     "Do you agree?"
+//     arrowFunc() 'alert("you agreed")';,
+//     arrowFunc() 'alert("you canceled the exection")';
+// 
