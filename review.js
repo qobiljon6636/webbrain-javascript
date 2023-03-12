9a904163e5cb6ab36d1c2a904ac4b0e9b511433b
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-12 21:55:07 +0500

    ...

--- review.js
+++ review.js
@@ -1,5 +1,4 @@
 
-
 // let name="webbrain";
 // let title="Lazizjon ";
 
@@ -8,7 +7,7 @@
 //     ["name"]:"nmadur",
 //     name:"webbrain",
 //     "name":"toshmat",
-    
+
 // }
 // console.log(obj[name]);
 // console.log(obj.name);
@@ -52,3 +51,155 @@
 // obj.name="eshmat"
 // console.log(user);
 // console.log(obj);
+//==================================
+
+
+
+// let user={
+//     name:"web",
+//     sayHi(){console.log(this);
+//     console.log(`HI, $ {this.name}`);}
+// }
+// user.sayHi()
+
+
+// function getName(){
+// //    this. name="web",
+//     console.log(new.target);
+// }
+
+// console.log(new getName("esh"));
+
+
+// let obj={
+//     name:"eshmat",
+//     title:"toshmat"
+// }
+// const {name,title}=obj;
+// console.log(title);
+// console.log(name);
+
+// let obj={
+//     name:"eshmat",
+//     title:"toshmat",
+//     child:{childname:"test"}
+// }
+// const {name,title,child:{childname}}=obj;
+// console.log(title);
+// console.log(name);
+// console.log(childname);
+
+
+// let obj ={
+//     name:"web",
+//     child:{
+//         childname:"eshmat"
+//     }
+// }
+// obj={...obj.child}
+// console.log(obj);
+
+
+// let user={
+//     name:"web",
+//     sayHi(){console.log(this.sayHi);
+//     console.log(`Hi, $ {this.name}`);}
+// }
+// user.sayHi()
+
+// function getName(){
+//     this.name="eshmat",
+//     console.log(this);
+// }
+// console.log(new getName().name);
+
+// let obj = {
+//     firstName: 'Qobiljon ',
+//     lastName: 'Sobirov',
+//     fullName() {
+//         return obj.firstName +  obj.lastName;
+//     }
+// }  
+
+
+// function Person(firstName, lastName) {
+//     this.firstName = firstName,
+//     this.lastName = lastName
+// }
+
+// let user1 = new Person('Qobiljon', 'Sobirov');
+// let user2 = new Person('Zuhriddin', 'Mahfiy');
+// console.log(user1.firstName);
+// console.log(user2);
+
+// let object = {
+//     firstName: 'Qobiljon',
+//     lastName: 'Sobirov'
+// };
+
+// const {firstName, lastName} = object;
+
+// const name = object.firstName;
+
+// console.log(name);
+
+//=====================================================
+
+
+
+
+// function getName(){
+//     return new.target;
+// }
+
+// function getSurname(){
+//     return getName;
+// }
+
+// let a=new getName;
+// let b=new getSurname;
+// console.log(a==b);
+
+//=================================
+
+
+// let name={
+//     step:0,
+//     up(){
+//         this.step++;
+//     },
+//     down(){
+//         this.step--;
+//     },
+//     showStep: function(){
+//         alert(this.step)
+//     }
+// }
+// name.up()
+
+// console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
