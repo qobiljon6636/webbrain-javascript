e3c9b2e9ca210bb9198ba477adc90f851a701539
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-15 22:52:59 +0500

    number

--- review.js
+++ review.js
@@ -25,7 +25,49 @@
 //bunda faqat butun qsmini olib qoladi qoldiq qismi ahamyatsiz bolib qoladi
 
 
+// console.log(Math.min(1,23,4,5,6,7,87,67));
+// console.log(Math.max(1,23,4,5,6,7,87,67));
 
+// console.log(Number(a));
+// console.log(parseInt(a))
+// console.log(parseFloat(a));
+
+//=================================================
+
+
+// let a=101;
+// let b=4321;
+
+// console.log(Math.abs(a-b));
+
+// =================================
+
+// let a="12.456432";
+
+// console.log(Math.trunc(a));
+//Bu faqat butun qismini olib berad "parsInt"dan farqi bun bu qiymatni ohirigacha tekshiradi ,agar orasida harf qatnashedigon bolsa nan qiymat qaytarad.
+
+
+// let a=2.543;
+
+// console.log(Math.ceil(a));
+
+//bunda qoldiq qismi bolsa unga 1 qiymat qoshib beradi :misol a=2.001=> qiymat 3 chiqad
+
+
+// let a=2.9999;
+
+// console.log(Math.floor(a));
+
+//bunda faqat butun qsmini olib qoladi qoldiq qismi ahamyatsiz bolib qoladi
+
+//======================================================================
+
+// function randomInteger(min,max){
+//     return min=1
+// }
+// randomInteger(1,50)
+// console.log(randomInteger());
 
 
 
