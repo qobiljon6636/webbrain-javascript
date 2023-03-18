7f617b15f62fb2ce0682e7e54a2f1dffa99849f3
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-03-18 15:58:01 +0500

    string

--- review.js
+++ review.js
@@ -1,76 +1,57 @@
-// let a=101;
-// let b=4321;
+//======================================
 
-// console.log(Math.abs(a-b));
+// let str="aziza"
+// function getName(){
 
-// =================================
-
-// let a="12.456432";
-
-// console.log(Math.trunc(a));
-//Bu faqat butun qismini olib berad "parsInt"dan farqi bun bu qiymatni ohirigacha tekshiradi ,agar orasida harf qatnashedigon bolsa nan qiymat qaytarad.
-
-
-// let a=2.543;
-
-// console.log(Math.ceil(a));
-
-//bunda qoldiq qismi bolsa unga 1 qiymat qoshib beradi :misol a=2.001=> qiymat 3 chiqad
-
-
-// let a=2.9999;
-
-// console.log(Math.floor(a));
-
-//bunda faqat butun qsmini olib qoladi qoldiq qismi ahamyatsiz bolib qoladi
-
-
-// console.log(Math.min(1,23,4,5,6,7,87,67));
-// console.log(Math.max(1,23,4,5,6,7,87,67));
-
-// console.log(Number(a));
-// console.log(parseInt(a))
-// console.log(parseFloat(a));
-
-//=================================================
-
-
-// let a=101;
-// let b=4321;
-
-// console.log(Math.abs(a-b));
-
-// =================================
-
-// let a="12.456432";
-
-// console.log(Math.trunc(a));
-//Bu faqat butun qismini olib berad "parsInt"dan farqi bun bu qiymatni ohirigacha tekshiradi ,agar orasida harf qatnashedigon bolsa nan qiymat qaytarad.
-
-
-// let a=2.543;
-
-// console.log(Math.ceil(a));
-
-//bunda qoldiq qismi bolsa unga 1 qiymat qoshib beradi :misol a=2.001=> qiymat 3 chiqad
-
-
-// let a=2.9999;
-
-// console.log(Math.floor(a));
-
-//bunda faqat butun qsmini olib qoladi qoldiq qismi ahamyatsiz bolib qoladi
-
-//======================================================================
-
-// function randomInteger(min,max){
-//     return min=1
+//     if(str===console.log(str.endsWith("aziza"))){
+//         console.log("true");
+// }  else{
+//     "folse"
 // }
-// randomInteger(1,50)
-// console.log(randomInteger());
+// }
+// getName()
+// //==========================================================
+
+// //=============================================
+
+// function get(str) {
+//     let res = ''
+//     for (i of str) {
+//         res+= i + i
+//         }
+//     console.log(res);
+// }
+// get("webbrain")
+
+// // ============================================
+
+// function GetCount(str) {
+//     let res = ''
+//     let count = 1
+//     for(i = 0; i < str.length; i++) {
+//         if (str[i] === str[i+1]) {
+//             count++
+//         } else {
+//             res += str[i]
+//             if (count > 1) {
+//                 res += count
+//                 count = 1
+//             }
+//         }
+//     }
+//     console.log(res);
+// }
+// GetCount('wwwweebbbraaaaiiinnnnnn')
 
 
-
+// function getName(str){
+//     let res=""
+//     for(let i=str.length-1;i>=0; i--){
+//         res=res+str[i];
+//     }
+//     return res===str
+// }
+// console.log(getName("kiyik"));
 
 
 
