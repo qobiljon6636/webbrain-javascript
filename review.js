364e022198667dfbf3ba5f705ebd531e7ca8471a
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-04-06 15:24:13 +0500

    recorsion

--- review.js
+++ review.js
@@ -1,172 +1,36 @@
-//=============================================
-
-// function getName(LT){
-  
-// let time=new Date();
-
-// console.log(`${time.getHours()}:${time.getMinutes()}`);
-
+// let n=5
+// function getName(n){
+//     if(n==1){
+//         return n;
+//     }else{
+//         return n+getName(n-1)
+//     }
 // }
-// getName("LT")
-//=========================================================
+// getName(n)
+// console.log(getName(n));
 
-// function getName(LTS){
-  
-//   let time=new Date();
-  
-//   console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
-  
-//   }
-//   getName("LTS")
-//=========================================================
-
-// function getName(L){
-  
-//   let time=new Date();
-  
-//   console.log(`${date.getDate()}:${date.getMonth()}:${date.getFullYear()}`);
-  
-//   }
-//   getName("L")
-
-//=======================================================
-
-// function getName(l){
-  
-//   let time=new Date();
-  
-//   console.log(`${date.getUTCDate()}:${date.getMonth()}:${date.getFullYear()}`);
-  
-//   }
-//   getName("l")
-//==========================================
-
-// function getName(LL){
-//    let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//    let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//   const date=new Date();
-  
-//   console.log(months[`${date.getMonth()}`],`${date.getDate()}:${date.getFullYear()}`);
-  
-//   }
-//   getName("LL")
-//========================================
-// function getName(LLL){
-//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//  const date=new Date();
- 
-//  console.log(months[`${date.getMonth()}`],`${date.getDate()}, ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
- 
-//  }
-//  getName("LLL")
-//=========================================
-// function getName(LLLL){
-//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//  const date=new Date();
- 
-//  console.log(weeaks[`${date.getDate()}`], months[`${date.getMonth()}`] ,` ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
- 
-//  }
-//  getName("LLLL")
-//=========================================================================
-
- getName("L")
-
-//=======================================================
-
-// function getName(l){
-  
-//   let time=new Date();
-  
-//   console.log(`${date.getUTCDate()}:${date.getMonth()}:${date.getFullYear()}`);
-  
-//   }
-//   getName("l")
-//==========================================
-
-// function getName(LL){
-//    let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//    let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//   const date=new Date();
-  
-//   console.log(months[`${date.getMonth()}`],`${date.getDate()}:${date.getFullYear()}`);
-  
-//   }
-//   getName("LL")
-//========================================
-// function getName(LLL){
-//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//  const date=new Date();
- 
-//  console.log(months[`${date.getMonth()}`],`${date.getDate()}, ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
- 
-//  }
-//  getName("LLL")
-//=========================================
-// function getName(LLLL){
-//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
-//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
-//  const date=new Date();
- 
-//  console.log(weeaks[`${date.getDate()}`], months[`${date.getMonth()}`] ,` ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
- 
-//  }
-//  getName("LLLL")
-
-// let title="name";
-// let obj={
-//   name:"webbrain",
-//   title:"IT Center",
-//   [title]:"Webbrain Academy",
-//   title,
+//===================================
+// let n=5
+// function getName(n){
+//     if(n==1){
+//         return n;
+//     }else{
+        // return n*getName(n-1)
+//     }
 // }
-
-// console.log(obj[title]); //=>"webbtain academy"
-// console.log(obj.title);//=> "name"
-// console.log(obj.name);//=>"webbtain academy"
-
-// let n=-5.654;
-// console.log(Math.floor(n));//=> -6
-// console.log(Math.trunc(n));//=> -5
-// console.log(Math.round(n));//=> -6
-// console.log(Math.sign(n));//=> -1
-// console.log(parseInt(n));//=> -5
-// console.log(Number(n));//=> -5.654
-  
-
-// let obj = {
-//   name: 'webbrain',
-//   lastName: 'Asilbek'
+// getName(n)
+// console.log(getName(n));
+//====================================
+// function fibonacci(n){
+//   let fib=[0,1];
+//   for(i=2;i<=n;i++){
+//     fib[i]=fib[i-2]+fib[i-1];
+//     console.log(fib[i]);
+//     fibonacci=fib[i]
+//   }
+//   return fibonacci;
 // }
-
-// let obj1 = [['name','webbrain'], ['lastName', 'Asilbek']];
-
-// console.log(fromEntries(obj));
-//======================================================
-
-// let title = 'name';
-
-// let obj = {
-//   name: 'Webbrain',
-//   title: 'It center',
-//   [title]: 'Webbrain Academy',
-//   title,
-// }
-
-// console.log(obj[title]);
-// console.log(obj.title);
-// console.log(obj.name);
-
-// let border = 'border';
-
-// const get = (a, b, c, ...rest) => {
-//   get.length = 'webbrain';
-// }
-
-// console.log(get.length);
+// console.log(fibonacci(7));
 
 
 
