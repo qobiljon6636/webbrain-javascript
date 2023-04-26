c3701f178465c2433bed1daed50e40dba965b160
qobiljon6636 <qobiljonsobirov20@gmail.com>
2023-04-26 23:21:30 +0500

    obj Math

--- review.js
+++ review.js
@@ -153,3 +153,94 @@ let n=5
 // let rabbit=new Rabbit("Rab");
 // alert(rabbit.hasOwnProperty());
 
+
+// function getName(LTS){
+  
+//   let time=new Date();
+  
+//   console.log(`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`);
+  
+//   }
+//   getName("LTS")
+//=========================================================
+
+// function getName(L){
+  
+//   let time=new Date();
+  
+//   console.log(`${date.getDate()}:${date.getMonth()}:${date.getFullYear()}`);
+  
+//   }
+//   getName("L")
+
+//=======================================================
+
+// function getName(l){
+  
+//   let time=new Date();
+  
+//   console.log(`${date.getUTCDate()}:${date.getMonth()}:${date.getFullYear()}`);
+  
+//   }
+//   getName("l")
+//==========================================
+
+// function getName(LL){
+//    let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
+//    let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
+//   const date=new Date();
+  
+//   console.log(months[`${date.getMonth()}`],`${date.getDate()}:${date.getFullYear()}`);
+  
+//   }
+//   getName("LL")
+//========================================
+// function getName(LLL){
+//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
+//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
+//  const date=new Date();
+ 
+//  console.log(months[`${date.getMonth()}`],`${date.getDate()}, ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
+ 
+//  }
+//  getName("LLL")
+//=========================================
+// function getName(LLLL){
+//   let months=["Januar","February","March","April","May","Juny","Junly","August","September","Octomber","November","December"]
+//   let weeaks=["Monday","Tuesday","Wednesday","Thusday","Friday","Saturday","Sunday"]
+//  const date=new Date();
+ 
+//  console.log(weeaks[`${date.getDate()}`], months[`${date.getMonth()}`] ,` ${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()} PM`);
+ 
+//  }
+//  getName("LLLL")
+
+// let title="name";
+// let obj={
+//   name:"webbrain",
+//   title:"IT Center",
+//   [title]:"Webbrain Academy",
+//   title,
+// }
+
+// console.log(obj[title]); //=>"webbtain academy"
+// console.log(obj.title);//=> "name"
+// console.log(obj.name);//=>"webbtain academy"
+
+// let n=-5.654;
+// console.log(Math.floor(n));//=> -6
+// console.log(Math.trunc(n));//=> -5
+// console.log(Math.round(n));//=> -6
+// console.log(Math.sign(n));//=> -1
+// console.log(parseInt(n));//=> -5
+// console.log(Number(n));//=> -5.654
+  
+
+// let obj = {
+//   name: 'webbrain',
+//   lastName: 'Asilbek'
+// }
+
+// let obj1 = [['name','webbrain'], ['lastName', 'Asilbek']];
+
+// console.log(fromEntries(obj));
